a5cee888f305cfa4e7fb3b230e817cfd233cf474
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-22 00:49:55 +0400

    formula scripts

---
+++ formula/formulabumppr.py
@@ -0,0 +1,16 @@
+import os
+
+a="""
+
+"""
+
+a=a.split("\n")
+for i in a:
+    if i!='':
+        l=i.split(":")
+        name=l[0]
+        m=l[1].split('==> ')[1]
+        print(name,m)
+        command="brew bump-formula-pr --version={} {}".format(m,name)
+        print(command)
+        os.system(command)
