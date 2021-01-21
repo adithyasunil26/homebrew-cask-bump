94717ad27171a72ace28c55ef369c591bf763f16
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-22 00:51:09 +0400

    latest cask scripts

--- caskbumppr.py
+++ caskbumppr.py
@@ -2,9 +2,6 @@ import os
 
 a="""
 
-
-
-
 """
 
 a=a.split("\n")
--- op.py
+++ op.py
@@ -8,8 +8,8 @@ for i in b:
   if i!='':
     l=i.split(": ")
     if l[0]!='':
-      if l[1] not in ['unversioned','latest','discontinued']:
-        if l[0] not in ['Error','Warning']:
+      if l[1] not in ['unversioned','latest','discontinued','deprecated','versioned','disabled']:
+        if l[0] not in ['Error','Warning','git','fatal']:
           m=l[1].split(' ==> ')[0]
           n=l[1].split(' ==> ')[1]
           if m!=n:
