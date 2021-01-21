de9ebf1da8922da416b62f9ae86f69b93d943a0e
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-22 00:50:11 +0400

    zap scripts v1

---
+++ zap script/scr.py
@@ -0,0 +1,11 @@
+import os
+from casks import a 
+
+b=a.split('\n')
+
+for i in b:
+  cmd='brew install --cask {}'.format(i)
+  print(cmd)
+  os.system(cmd)
+  os.system('open -a {}'.format(i))
+  os.system('brew cask createzap {}'.format(i)+'> ./output.txt')
