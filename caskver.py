64e36b0f1526e2f03f3993cb6ee50fce720e2cd0
adithyasunil26 <adithyasunil26@gmail.com>
2021-01-25 02:09:37 +0400

    updated

--- caskver.py
+++ caskver.py
@@ -3,14 +3,14 @@ import re
 #from caskslist1 import a
 #from caskslist2 import b
 #from caskslist3 import c
-from caskslist4 import d
-#from caskslist5 import e
+#from caskslist4 import d
+from caskslist5 import e
 #from caskslist6 import f
 #from caskslist7 import g
 
-#1 2 3 done 24/01/21
+#1 2 3 4 done 25/01/21
 
-b=d
+b=e
 
 b=b.replace("\n","\t").replace(" ","\t").split("\t")
 c=[]
